5bb0f3348fe039ad002d10ad442c5413f4f02f81
Elisa <elisacho81@gmail.com>
2018-11-06 14:02:40 +0900

    새주문 버튼 intent to orderActivity.class

--- app/src/main/java/com/jmk/edu/jjcafeapp/MainActivity.java
+++ app/src/main/java/com/jmk/edu/jjcafeapp/MainActivity.java
@@ -1,21 +1,38 @@
 package com.jmk.edu.jjcafeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-       //comment three
+        //comment three
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button buttonNewOrder = (Button) findViewById(R.id.buttonNewOrder);
+        buttonNewOrder.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), OrderActivity.class);
+                view.getContext().startActivity(intent);}
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
