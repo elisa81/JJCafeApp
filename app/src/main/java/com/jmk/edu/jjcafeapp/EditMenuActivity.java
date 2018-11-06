f260615f73dfe4ceef6bf7864aa817ea52185a6d
deartune <deartune@gmail.com>
2018-11-06 12:23:04 +0900

    comment one

--- app/src/main/java/com/jmk/edu/jjcafeapp/EditMenuActivity.java
+++ app/src/main/java/com/jmk/edu/jjcafeapp/EditMenuActivity.java
@@ -3,15 +3,11 @@ package com.jmk.edu.jjcafeapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EditMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //comment one
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // First add commit
-
+        setContentView(R.layout.activity_edit_menu);
     }
 }
