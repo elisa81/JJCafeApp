084184b0321bf3b4d2d23c145156039efbaeff23
Elisa <elisacho81@gmail.com>
2018-11-07 14:37:20 +0900

    MainActivity.java 수정

--- app/src/main/java/com/jmk/edu/jjcafeapp/MainActivity.java
+++ app/src/main/java/com/jmk/edu/jjcafeapp/MainActivity.java
@@ -6,11 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
@@ -19,28 +24,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     MyDBOpenHelper dbHelper;
     SQLiteDatabase mDB;
     Button buttonNewOrder;
-    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         dbHelper = new MyDBOpenHelper(this, "jjcafe.db", null, 1);
         mDB = dbHelper.getWritableDatabase();
-
-        String[] FRUITS = new String[] { "Apple", "Avocado", "Banana","Blueberry"};
-        listView = (ListView) findViewById(R.id.resultList);
-        ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<String>(this, R.layout.listview_single_column,FRUITS);
-        listView.setAdapter(arrayAdapter);
-        listView.setOnItemClickListener(this);
-
-
 
         buttonNewOrder= findViewById(R.id.buttonNewOrder);
         buttonNewOrder.setOnClickListener(this);
 
+
+        Bundle bundle = getIntent().getExtras();
+        ((TextView) findViewById(R.id.textViewOrdered)).setText(bundle.getString("Menu", "No Data"));
+
+//        ((Button) findViewById(R.id.buttonSum)).setOnClickListener(this);
+//        ((Button) findViewById(R.id.buttonCancel)).setOnClickListener(this);
 
     }
 
